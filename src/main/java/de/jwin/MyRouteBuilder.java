35a13bbb15b7c77ae5be2ba50052ab9dafee897b
clojj <jwin1968@gmail.com>
2015-07-31 22:00:34 +0200

    switch to logback

--- src/main/java/de/jwin/MyRouteBuilder.java
+++ src/main/java/de/jwin/MyRouteBuilder.java
@@ -1,20 +1,27 @@
 package de.jwin;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A Camel Java DSL Router
  */
 public class MyRouteBuilder extends RouteBuilder {
 
+    Logger logger = LoggerFactory.getLogger(MyRouteBuilder.class);
+
     /**
      * Let's configure the Camel routing rules using Java code...
      */
     public void configure() {
 
+        logger.debug("configure routes...");
+
         // here is a sample which processes the input files
         // (leaving them in place - see the 'noop' flag)
         // then performs content based routing on the message using XPath
+
         from("file:src/data?noop=true")
             .choice()
                 .when(xpath("/person/city = 'London'"))
